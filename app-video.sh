95b5a628a192408521b3a46dca5d7e3077395f92
fangdown <409951398@qq.com>
2020-05-03 17:03:06 +0800

    '增加video构建'

---
+++ app-video.sh
@@ -0,0 +1,4 @@
+# 打开网站根目录
+cd ../app-video
+# 执行构建
+sh build.sh
--- webhook.js
+++ webhook.js
@@ -27,33 +27,39 @@
 //     // run_cmd('sh', ['./deploy.sh',event.payload.repository.name], function(text){ console.log(text) });
 // })
 
-
 var http = require('http')
 var createHandler = require('node-github-webhook')
-var handler = createHandler([ // 多个仓库
+var handler = createHandler([
+  // 多个仓库
   {
     path: '/app-api',
-    secret: 'fangdown'
+    secret: 'fangdown',
   },
   {
     path: '/app-git123',
-    secret: 'fangdown'
+    secret: 'fangdown',
   },
   {
     path: '/app-admin-vue',
-    secret: 'fangdown'
-  }
+    secret: 'fangdown',
+  },
+  {
+    path: '/app-video',
+    secret: 'fangdown',
+  },
 ])
 // var handler = createHandler({ path: '/webhook1', secret: 'secret1' }) // 单个仓库
 
-http.createServer(function (req, res) {
-  handler(req, res, function (err) { 
-    res.statusCode = 404
-    res.end('no such location')
+http
+  .createServer(function (req, res) {
+    handler(req, res, function (err) {
+      res.statusCode = 404
+      res.end('no such location')
+    })
   })
-}).listen(11009,()=>{
-  console.log('webhook start on port 11009')
-})
+  .listen(11009, () => {
+    console.log('webhook start on port 11009')
+  })
 
 handler.on('error', function (err) {
   console.error('Error:', err.message)
@@ -68,20 +74,49 @@ handler.on('push', function (event) {
   console.log(`${event.path}`)
   switch (event.path) {
     case '/app-api':
-      runCmd('sh', ['./app-api.sh', event.payload.repository.name], function (text) { console.log(text) })
+      runCmd('sh', ['./app-api.sh', event.payload.repository.name], function (
+        text
+      ) {
+        console.log(text)
+      })
       break
     case '/app-git123':
       const branch = event.payload.ref.replace('refs/heads/', '')
-      if(branch === 'master'){
+      if (branch === 'master') {
         console.log(`执行master`)
-        runCmd('sh', ['./app-git123-home.sh', event.payload.repository.name], function (text) { console.log(text) })
-      } else if(branch ==='blog'){
+        runCmd(
+          'sh',
+          ['./app-git123-home.sh', event.payload.repository.name],
+          function (text) {
+            console.log(text)
+          }
+        )
+      } else if (branch === 'blog') {
         console.log(`执行blog`)
-        runCmd('sh', ['./app-git123-blog.sh', event.payload.repository.name], function (text) { console.log(text) })
+        runCmd(
+          'sh',
+          ['./app-git123-blog.sh', event.payload.repository.name],
+          function (text) {
+            console.log(text)
+          }
+        )
       }
       break
     case '/app-admin-vue':
-      runCmd('sh', ['./app-admin-vue.sh', event.payload.repository.name], function (text) { console.log(text) })
+      runCmd(
+        'sh',
+        ['./app-admin-vue.sh', event.payload.repository.name],
+        function (text) {
+          console.log(text)
+        }
+      )
+      break
+    case '/app-video':
+      runCmd('sh', ['./app-video.sh', event.payload.repository.name], function (
+        text
+      ) {
+        console.log(text)
+      })
       break
     default:
       // 处理其他
@@ -89,7 +124,7 @@ handler.on('push', function (event) {
   }
 })
 
-function runCmd (cmd, args, callback) {
+function runCmd(cmd, args, callback) {
   var spawn = require('child_process').spawn
   var child = spawn(cmd, args)
   var resp = ''
